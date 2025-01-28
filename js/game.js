2e18138be21143bcd400c54972d6a5e098911eb2
Niclas Holzhey <niclas@MacBook-Pro-von-Niclas.local>
2025-01-28 20:46:03 +0100

    Update - Code

--- js/game.js
+++ js/game.js
@@ -115,12 +115,13 @@ function unmuteSound() {
 }
 
 function showAbout() {
-  if (document.getElementsByTagName('footer')[0].style.display == '' || document.getElementsByTagName('footer')[0].style.display == 'none') {
-    document.getElementsByTagName('footer')[0].style.display = 'flex';
+  if (document.getElementsByTagName('section')[0].style.display == '' || document.getElementsByTagName('section')[0].style.display == 'none') {
+    document.getElementsByTagName('section')[0].style.display = 'flex';
     document.getElementById('more-about-arrow').style.rotate = '180deg';
     document.getElementById('more-about-arrow').style.filter = 'drop-shadow(0px -4px 6px black)';
+    document.getElementsByTagName('section')[0].scrollIntoView({ behavior: 'smooth', flex: 'end' });
   } else {
-    document.getElementsByTagName('footer')[0].style.display = 'none';
+    document.getElementsByTagName('section')[0].style.display = 'none';
     document.getElementById('more-about-arrow').style.rotate = '0deg';
     document.getElementById('more-about-arrow').style.filter = '';
   }
